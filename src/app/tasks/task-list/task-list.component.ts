f9e10dfd0b2b95153c05fc62a1a002f8f2eb074f
Thaynara Araújo <thaynara_araujo1@yahoo.com.br>
2022-03-12 20:34:43 -0300

    configuração de classe e service

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -1,15 +1,17 @@
+import { Task } from '../shared/task';
+import { TaskService } from './../shared/task.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  styleUrls: ['./task-list.component.css'],
 })
 export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.tasks = this.taskService.getAll();
   }
-
 }
